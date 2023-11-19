b746ecef163e1cb22da7e9a5a35b66f1b154901b
mo sayed <grooveymo@gmail.com>
2023-11-19 18:02:53 +0000

    feat: added client/server pages

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,13 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <Card className="m-8">
       <CardContent className="flex justify-end">
         <div className="w-1/4 mt-4 flex justify-between">
-          <Button>Client-side Filtering</Button>
-          <Button variant={'secondary'}>Server-side Filtering</Button>
+          <Link href="/client">Client-side Filtering</Link>
+          <Link href="/server">Server-side Filtering</Link>
         </div>
       </CardContent>
     </Card>
