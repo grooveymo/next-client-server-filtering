b21a6b8f485542999bb7fe93aad577556e6d1242
mo sayed <grooveymo@gmail.com>
2023-12-22 17:27:26 +0000

    feat: fixed badge colours

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -4,13 +4,36 @@ export default function ClientPage() {
   return (
     <div>
       <h1>Client Page</h1>
-      <InvoiceCard
-        id="1"
-        name="name"
-        value={100}
-        dueDate="2021-10-10"
-        status="draft"
-      />
+      <div className="flex flex-col gap-4">
+        <InvoiceCard
+          id="INV001"
+          name="name"
+          value={100}
+          dueDate="2021-10-10"
+          status="draft"
+        />
+        <InvoiceCard
+          id="IN002"
+          name="name2"
+          value={200}
+          dueDate="2023-10-10"
+          status="pending"
+        />
+        <InvoiceCard
+          id="IN003"
+          name="name3"
+          value={300}
+          dueDate="2023-10-10"
+          status="paid"
+        />
+        <InvoiceCard
+          id="IN004"
+          name="name4"
+          value={400}
+          dueDate="2023-04-12"
+          status="overdue"
+        />
+      </div>
     </div>
   );
 }
--- components/badge/badge.tsx
+++ components/badge/badge.tsx
@@ -5,18 +5,18 @@ import { InvoiceStatus } from '@/types/invoice-status';
 
 const BadgeVariants = cva(
   /* badge base style */
-  'rounded rounded-lg shadow-md w-[100px] text-center py-4 -mt-4',
+  'rounded rounded-lg shadow-md w-[100px] text-center font-bold py-4 -mt-4',
   {
     variants: {
       /* badge colors */
       intent: {
         draft:
-          'bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 dark:bg-yellow-900 dark:text-yellow-300 hover:bg-yellow-900',
+          'text-xs font-medium me-2 px-2.5 py-0.5 dark:bg-yellow-100 dark:text-yellow-800 hover:bg-yellow-900',
         pending:
-          'bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 dark:bg-blue-900 dark:text-blue-300 hover:bg-red-600',
-        paid: 'bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 dark:bg-green-900 dark:text-green-300 hover:bg-green-900',
+          'text-xs font-medium me-2 px-2.5 py-0.5 dark:bg-blue-100 dark:text-blue-800 hover:bg-red-600',
+        paid: 'text-xs font-medium me-2 px-2.5 py-0.5 dark:bg-green-100 dark:text-green-800 hover:bg-green-900',
         overdue:
-          'bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 dark:bg-red-900 dark:text-red-300 hover:bg-red-600',
+          'text-xs font-medium me-2 px-2.5 py-0.5 dark:bg-red-100 dark:text-red-800 hover:bg-red-600',
       },
 
       /* badge roundness */
@@ -40,9 +40,9 @@ export type BadgeBaseProps = VariantProps<typeof BadgeVariants>;
 export interface BadgeProps extends BadgeBaseProps {
   status: InvoiceStatus;
 }
-const Badge = ({ status, intent, roundness }: BadgeProps) => {
+const Badge = ({ status, roundness }: BadgeProps) => {
   return (
-    <div className={BadgeVariants({ intent, roundness })}>
+    <div className={BadgeVariants({ intent: status, roundness })}>
       <span className="capitalize">{status}</span>
     </div>
   );
