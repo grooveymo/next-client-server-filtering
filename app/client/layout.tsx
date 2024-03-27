e4dbdd761233581154f968ce2f74c27ebaa30450
mo sayed <grooveymo@gmail.com>
2024-03-27 17:18:49 +0000

    feat: highlighted client/server buttons

--- app/client/layout.tsx
+++ app/client/layout.tsx
@@ -7,18 +7,19 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <h1>Client Layout</h1>
       <ReactQueryProvider>
-      <main>
-        <section className="border border-dashed border-green-900 w-full flex flex-row">
-          <aside className="border border-dashed border-red-900 w-2/12 h-full">
-            <section className="mt-16">
-              <Filter />
-            </section>
-          </aside>
-          <article className="w-10/12 h-full border border-1 border-blue-400">
-            {children}
-          </article>
-        </section>
-      </main>
+        <main>
+          {/* <section className="border border-dashed border-green-900 w-full flex flex-row"> */}
+          <section className="w-full flex flex-row">
+            {/* <aside className="border border-dashed border-red-900 w-2/12 h-full"> */}
+            <aside className="w-2/12 h-full">
+              <section className="mt-16">
+                <Filter />
+              </section>
+            </aside>
+            {/* <article className="w-10/12 h-full border border-1 border-blue-400"> */}
+            <article className="w-10/12 h-full ">{children}</article>
+          </section>
+        </main>
       </ReactQueryProvider>
     </>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,36 @@
+'use client';
 import { Card, CardContent } from '@/components/ui/card';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+  const pathname = usePathname();
+  console.log('🚀 ~ Navbar ~ pathname:', pathname);
+
   return (
     <Card className="m-8">
       <CardContent className="flex justify-end">
         <div className="w-1/4 mt-4 flex justify-between">
-          <Link href="/client">Client-side Filtering</Link>
-          <Link href="/server">Server-side Filtering</Link>
+          <Link
+            href="/client"
+            className={
+              pathname === '/client'
+                ? 'border rounded-lg border-gray-700 p-4 bg-slate-700'
+                : 'border rounded-lg border-gray-100 p-4 bg-slate-700 opacity-10'
+            }
+          >
+            Client-side Filtering
+          </Link>
+          <Link
+            href="/server"
+            className={
+              pathname === '/server'
+                ? 'border rounded-lg border-gray-700 p-4 bg-slate-700'
+                : 'border rounded-lg border-gray-100 p-4 bg-slate-700 opacity-10'
+            }
+          >
+            Server-side Filtering
+          </Link>
         </div>
       </CardContent>
     </Card>
