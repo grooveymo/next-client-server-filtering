a7983c92f57f1d06edea4f83afc1ebc6e3e272f7
mo sayed <grooveymo@gmail.com>
2024-03-27 15:36:44 +0000

    feat: installed shadcn input + wired in form

--- components/filter/filter.tsx
+++ components/filter/filter.tsx
@@ -1,81 +1,130 @@
 'use client';
 
-import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Button } from '@/components/ui/button';
 import { Checkbox } from '../ui/checkbox';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '../ui/input';
+
+const items = [
+  {
+    id: 'draft',
+    label: 'Draft',
+  },
+  {
+    id: 'pending',
+    label: 'Pending',
+  },
+  {
+    id: 'paid',
+    label: 'Paid',
+  },
+  {
+    id: 'overdue',
+    label: 'Overdue',
+  },
+] as const;
+
+const FormSchema = z.object({
+  search: z.string(),
+  items: z.array(z.string()).refine((value) => value.some((item) => item), {
+    message: 'You have to select at least one item.',
+  }),
+});
 
 const Filter = () => {
-  const [filters, setFilters] = useState({
-    draft: false,
-    pending: false,
-    paid: false,
-    overdue: false,
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      items: ['overdue'],
+      search: '',
+    },
   });
-  const handleCheckedChange = (value: boolean) => {
-    console.log(
-      'checked => ',
-      value
-      // e.target?.id,
-      // ' => ',
-      // e.target?.checked,
-      // ' => ',
-    );
-  };
 
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log('>>> onSubmit => data: ', data);
+  }
   return (
     <div className="flex flex-col justify-center">
       <div className="mx-8 border border-slate-100 rounded-2xl p-8 flex flex-col justify-between w-[250px]">
-        <div data-testid="search">
-          <label htmlFor="search">Search</label>
-          <input id="search" type="text" />
-        </div>
-        <div data-testid="quick filter" className="mt-8 flex flex-col">
-          <h2 className="text-2xl font-bold align-middle mb-4">Filter</h2>
-          <div className="ml-4 flex align-middle flex-col gap-6 justify-between w-[400px]">
-            <div className="flex items-center space-x-2">
-              {/* onClick={handleCheckedChange} */}
-              <Checkbox
-                id="draft"
-                name="draft"
-                // onCheckedChange={(e) => console.log('>>> xxx e: ', e)}
-                // onCheckedChange={handleCheckedChange}
-                onClick={(e) => console.log('>>> xxx', e.currentTarget.name)}
-              />
-              <label
-                htmlFor="draft"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                Draft
-              </label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <Checkbox id="pending" />
-              <label
-                htmlFor="pending"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                Pending
-              </label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <Checkbox id="paid" />
-              <label
-                htmlFor="paid"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                Paid
-              </label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <Checkbox id="overdue" />
-              <label
-                htmlFor="overdue"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                Overdue
-              </label>
-            </div>
-          </div>
-        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="items"
+              render={() => (
+                <FormItem>
+                  <div className="mb-4">
+                    <FormField
+                      control={form.control}
+                      name="search"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <Input placeholder="Simpson" {...field} />
+                          </FormControl>
+                          <FormDescription>
+                            Search for an invoice by name
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    {/* <FormLabel className="text-base">Sidebar</FormLabel>
+                    <FormDescription>
+                      Select the items you want to display in the sidebar.
+                    </FormDescription> */}
+                  </div>
+                  {items.map((item) => (
+                    <FormField
+                      key={item.id}
+                      control={form.control}
+                      name="items"
+                      render={({ field }) => {
+                        return (
+                          <FormItem
+                            key={item.id}
+                            className="flex flex-row items-start space-x-3 space-y-0"
+                          >
+                            <FormControl>
+                              <Checkbox
+                                checked={field.value?.includes(item.id)}
+                                onCheckedChange={(checked) => {
+                                  return checked
+                                    ? field.onChange([...field.value, item.id])
+                                    : field.onChange(
+                                        field.value?.filter(
+                                          (value) => value !== item.id
+                                        )
+                                      );
+                                }}
+                              />
+                            </FormControl>
+                            <FormLabel className="font-normal">
+                              {item.label}
+                            </FormLabel>
+                          </FormItem>
+                        );
+                      }}
+                    />
+                  ))}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
